638b3982c212f8a461cfd0b055c3be9fa6d521df
Austin Davis-Richardson <harekrishna@gmail.com>
2016-06-13 17:30:54 -0700

    added CROP

--- lib/decoder/matrix_plate_decoder.rb
+++ lib/decoder/matrix_plate_decoder.rb
@@ -1,27 +1,41 @@
 require 'rmagick'
 require 'Rdmtx'
 require 'pry'
+require 'opencv'
+
+class ScanFailed < Exception
+end
 
 module Decoder
-  class GSPlateDecoder < Base
+  class MatrixPlateDecoder < Base
 
-    TIMEOUT = 800 # ms
+    TIMEOUT = 100 # ms
 
     # geometric constants
-    S_x = 530
-    S_y = 800
+    S_x  = 530
+    S_y  = 800
     S_bx = 26
     S_by = 34
-    S_w = 180
-    S_h = 180
+    S_w  = 180
+    S_h  = 180
+    CROP = 20
 
     private
 
     def scan_plate
-      rdmtx = Rdmtx.new
       well_images.map do |i, j, well|
-        rdmtx.decode(well, TIMEOUT).first
+        value = scan_well(well)
+        well.write("#{i}-#{j}-#{value || 'nil'}.jpeg")
+        value
       end
+    end
+
+    def rdmtx
+      @rdmtx ||= Rdmtx.new
+    end
+
+    def scan_well well
+      rdmtx.decode(well, TIMEOUT).first
     end
 
     # yield wells as separate images
@@ -31,7 +45,11 @@ module Decoder
           8.times.map do |col|
             x = S_x + (col * S_bx) + (col * S_w)
             y = S_y + (row * S_by) + (row * S_h)
-            cell = image.crop(x, y, S_w, S_h)
+            cell = image.crop(x + CROP,
+                              y + CROP,
+                              S_w - CROP,
+                              S_h - CROP)
+
             enum.yield(row, col, cell)
           end
         end
