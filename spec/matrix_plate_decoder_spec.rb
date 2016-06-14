85eb06548345992fe029548cf84aa40f59cc4ee9
Austin Davis-Richardson <harekrishna@gmail.com>
2016-06-14 10:48:35 -0700

    changed test image

--- spec/matrix_plate_decoder_spec.rb
+++ spec/matrix_plate_decoder_spec.rb
@@ -1,13 +1,13 @@
 require 'spec_helper'
 
 describe Decoder::MatrixPlateDecoder do
-  let(:decoder) { Decoder::MatrixPlateDecoder.new('./spec/data/gsplate-600-color.tiff') }
+  let(:decoder) { Decoder::MatrixPlateDecoder.new('./spec/data/600-dpi-1shotplate.jpeg') }
 
   it 'can be created' do
     expect { decoder }.to_not raise_error
   end
 
   it '#codes returns an array' do
-    expect(decoder.codes.count(nil)).to eq(35)
+    expect(decoder.codes.count(nil)).to eq(7)
   end
 end
